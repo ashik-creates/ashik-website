b2264e746d7d3246368459f52dc65246ef7732e3
ashik-creates <ashikorrahman8900@gmail.com>
2024-07-14 12:32:50 +0000

    added dynamyc data and api

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    "id": 1,
+    "title": "Data Analyst",
+    "location": "London"
+  },
+  {
+    "id": 2,
+    "title": "Computer Scientist",
+    "location": "New York"
+  },
+  {
+    "id": 3,
+    "title": "Hacker",
+    "location": "Dubai"
+  },
+  {
+    "id": 4,
+    "title": "Graphic Designer",
+    "location": "Dhaka"
+  }
+]
+
 @app.route("/")
 def home():
-  return render_template("index.html")
+  return render_template("index.html", jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',port=8080, debug=True)
