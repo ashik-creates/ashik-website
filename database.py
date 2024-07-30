6d236b265b8ef047fcc4486b263dd5e5fbc5ff54
ashik-creates <ashikorrahman8900@gmail.com>
2024-07-30 11:22:27 +0000

    added dynamic pages

--- database.py
+++ database.py
@@ -34,3 +34,14 @@ def load_jobs_from_db():
     for row in result.all():
         jobs.append(row._asdict())
   return jobs
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(
+            text(f"select * from jobs where id = {id}"))
+        row = result.all()
+        if row == 0:
+            return None
+        else:
+            return row[0]._asdict()
+                        
